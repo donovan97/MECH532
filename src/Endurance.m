634cefc2693a4b05b29589736607c7e99edfc7cb
lmstrm <61624861+lmstrm@users.noreply.github.com>
2020-02-29 10:53:46 -0500

    Update Endurance.m

--- src/Endurance.m
+++ src/Endurance.m
@@ -1,4 +1,4 @@
-function [E] = Endurance(phi,CL,CD,m,S,rho)
+function [E] = Endurance(phi,CL,CD,m,S,rho,g)
 % Calcul l'endurance en s durant la mission
 %   htypothèse: rho=rho0= 1.225 kg/m, altitude de départ=200m
 h=200;  %m
@@ -7,7 +7,7 @@ Vt=sqrt(2*n*m*g/(rho*S*CL));
 V=sqrt(2*m*g/(rho*S*CL));
 Vst=sqrt(2*m*g/(rho*S))*(n/CL)^(3/2)*CD;
 Vs=n^(-3/2)*Vst;
-R=(Vt^2)/(g(n^2-1));
+R=(Vt^2)/(g*(n^2-1));
 w=sqrt(2*n*m*g/(rho*S*CL))/R;
 t_t=pi/w;
 h_t=t_t*Vst;

5842cec4d837dc44857cadab736d0c3edeb5bda6
lmstrm <61624861+lmstrm@users.noreply.github.com>
2020-02-29 10:00:13 -0500

    Create Endurance.m

---
+++ src/Endurance.m
@@ -0,0 +1,10 @@
+function [E] = Endurance(phi,CL,CD,m,S,rho)
+% Calcul l'endurance en s durant la mission
+%   htypothèse: rho=rho0= 1.225 kg/m, altitude de départ=200m
+h=200;  %m
+Vt=sqrt(2*n*m*g/(rho*S*CL));
+V=sqrt(2*m*g/(rho*S*CL));
+
+
+end
+

8ba9202c861c06a0cd8fb17f178ad0a1438502ef
Audrey3131 <61624668+Audrey3131@users.noreply.github.com>
2020-03-13 12:43:15 -0400

    Update Airfoils_analysis.m

--- src/Airfoils_analysis.m
+++ src/Airfoils_analysis.m
@@ -6,73 +6,82 @@ clc
 % Airfoils analysis
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%Chargement des profils
-Profils  
 
-%Data
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% WING
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%Plug the right numbers below
+
+%Airfoil : MH32
+
+figure(1)
+MH32 = load('MH32.dat')
+plot(MH32(:,1),smooth(MH32(:,2)))
+title('MH32 Airfoil')
+
+rho0 = 1.225;
+c = 0.1; %comparative table
+mu = 18e-6;
+u = 8; %which velocity is required in cruise
+Re = rho0*u*c/mu;
+
+% Analyse XFLR
+
+clmax = 1.1;
+CLmax = 0.9*clmax;
+
+% Analyse théorique  
 
 e1 = 0.87; % Oswald coeff
-M = 0.3; % Mach number
-A = 6; % Aspect ratio
-S = 40; %m2
+M = 0.3; % Mach number : a changer
+A = 6; % Aspect ratio : a changer
+S = 40; %m2 : a changer
 b =  sqrt(S*A);
-c = S/b;
-alphachoose = 4; % degrees
+c = S/b; 
+alphachoose = 4; % degrees_ a changer
 
-%Reynolds
+tc = 0.087;
+xc = 0.302;
 
-T_200 = 250; % Temperature (200m, K)
-rho_200 = 1.225; %kg/m3
-a_200 = 300; % Speed of sound
-visdyn_200 = 18*10^-6; % Viscosité dynamique air (18000ft)
-Re = rho_200*M*a_200*c/visdyn_200;
+alphap = [-5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 12];
+Clp = [-4.003 -0.3205 -0.2218 -0.1218 -0.0384 0.0413 0.1256 0.3366 0.4946 0.6596 0.8006 0.9025 0.9854 1.0499 1.0991 0.9141];
 
-%Loop
+%a0
+t0 = round(1/3*length(alphap));
+y0 = Clp(t0);
+y1 = Clp(length(alphap)-t0);
+x0 = alphap(t0);
+x1 = alphap(length(alphap)-t0);
+a0 = (y1-y0)/(x1-x0); %a0 en incompressible
+a3d = a0/(sqrt(1-M^2)+a0/(pi*e1*A)); % a0 en 3d
 
-for i = 1:1:length(legends)
-    alphap = cell2mat(alpha(i)); % abscisses du profil i
-    Clp = cell2mat(Cl(i)); % ordonnées du profil i
-    temp = legends(i); % legendes du profil i
+%alpha for Cl =0
+[trash ,indice] = min(abs(Clp));
+alphal0 = alphap(indice);
+
+%Final CL 
+CL = a3d*(alphachoose - alphal0);
+
+%a0 slope to ensure the good approximation
     
-    %a0
-    t0 = round(1/3*length(alphap));
-    y0 = Clp(t0);
-    y1 = Clp(length(alphap)-t0);
-    x0 = alphap(t0);
-    x1 = alphap(length(alphap)-t0);
-    a0 = (y1-y0)/(x1-x0); %a0 en incompressible
+droite_pente = alphap.*a0;
+oo = Clp(t0)-droite_pente(t0);
+droite_pente = droite_pente + oo;
+
+% Calcul de Cd0
+
+C_f = 0.455/(((log10(Re))^2.58)*(1+0.144*M^2)^0.65);
+FF = (1+(0.6/xc)*tc+100*(tc)^4)*(1.34*M^0.18);
+Q = 1;
+Swet = S*(1.977+0.52*tc);
+Cd0 = C_f*FF*Q*Swet/S;
     
-    a3d = a0/(sqrt(1-M^2)+a0/(pi*e1*A)); % a0 en 3d
-    
-    %alpha for Cl =0
-    [trash ,indice] = min(abs(Clp));
-    alphal0 = alphap(indice);
-    
-    %Final CL
-    
-    CL = a3d*(alphachoose - alphal0);
-    
-    %a0 slope to ensure the good approximation
-    
-    droite_pente = alphap.*a0;
-    oo = Clp(t0)-droite_pente(t0);
-    droite_pente = droite_pente + oo;
-    
-    %Cd0
-    
-    C_f = 0.455/(((log10(Re))^2.58)*(1+0.144*M^2)^0.65);
-    FF = (1+(0.6/temp{1}{4})*temp{1}{3}+100*(temp{1}{3})^4)*(1.34*M^0.18);
-    Q = 1;
-    Swet = S*(1.977+0.52*temp{1}{3});
-    Cd0 = C_f*FF*Q*Swet/S;
-    
-    %Plot
-    figure(i)
-    hold on
-    plot(alphap, Clp)
-    plot(alphap, droite_pente)
-    title(strcat(temp{1}{1}, ' Re = ',temp{1}{2}, ' alpha = ', num2str(alphachoose)))
-    legend(strcat('CL = ', num2str(CL)), strcat('Cd0 = ', num2str(Cd0)))
-end
+%Plot
+figure(2)
+hold on
+plot(alphap, Clp)
+plot(alphap, droite_pente)
+title(strcat('MH32', ' Re = ',num2str(Re), ' alpha = ', num2str(alphachoose)))
+legend(strcat('CL = ', num2str(CL)), strcat('Cd0 = ', num2str(Cd0)))
+xlabel('alpha (°)')
+yabel('Cl')

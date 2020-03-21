36f18950750b4fe5448486bcf2e3ba20a3c66b51
donovan97 <48806319+donovan97@users.noreply.github.com>
2020-03-20 22:03:08 -0400

    Fixed errors in param_def.m

--- src/Param_def.m
+++ src/Param_def.m
@@ -1,13 +1,16 @@
 %Constants
 
 parameters.Wws     		= 7790 ;  							% [kg]           Weight of wing structure
-parameters.Wfus    		= 31160 ; 							% [kg]           Weight of the fuselage (Tru Sim)
+parameters.Wfus    		= 31160 ; 							% [kg]           Weight of the fuselage
 parameters.Wto     		= parameters.Wws +parameters.Wfus   % [kg]           Total aircraft weight
-parameters.E       		= 73;       						% [GPa]          Young's modulus AL2024 https://amesweb.info/Materials/Youngs-Modulus-of-Aluminum.aspx
-parameters.sigma_yield  = 324;          					% [MPa]          Yield stress of AL2024 http://asm.matweb.com/search/SpecificMaterial.asp?bassnum=MA2024T4
 parameters.rho          = 1.225;                            % [kg/m^3]       masse volumique   
-parameters.mu           = 18*10E-6 ;                        % [Pa/s]         viscositï¿½ dynamique   
+parameters.mu           = 18*10E-6 ;                        % [Pa/s]         viscosité dynamique   
 parameters.e            = 0.87 ;                            %                coefficient d'Oswald   
+
+%Material properties of EPP foam
+
+parameters.Sut          = 0.38 ;                            % [MPa]          Ultimate tensile strength of EPP foam
+parameters.Suc          = 0.44 ;                            % [MPa]          Ultimate compressive strength of EPP foam
 
 %Parameters
 
@@ -20,47 +23,35 @@ parameters.t       		= 0.12*parameters.Co;  				% [m]            Max thickness o
 parameters.h       		= 0.02*parameters.Co;      			% [m]            max camber of NACA 
 parameters.n       		= 1 ;       						% [-]            load factor
 parameters.g       		= 9.81;     						% [m/s^2)        gravitational constant
-parameters.dihedral     = 0 ;           					% [rad]          Dihedral angle of the wing
-parameters.x            = linspace(0, parameters.L,1000);   % [-]            1000 discrete points for wing
-parameters.drag         = 15 ;           					% [N]            Total drag force on the glider
-
-%Material properties of EPP foam
-
-parameters.Sut          = 0.38 ;                            % [MPa]          Ultimate tensile strength of EPP foam
-parameters.Suc          = 0.44 ;                            % [MPa]          Ultimate compressive strength of EPP foam
-
-
-
 parameters.dihedralw    = 0 ;           					% [rad]          Dihedral angle of the wing
 parameters.x            = linspace(0, parameters.L,1000);
 parameters.drag         = 15 ;           					% [N]            Total drag force on the glider
 parameters.u            = 8 ;                               % [m/s]          Speed of glider   
-parameters.clmax        = 1.1 ;                             %                clmax 2D    
-%Les paramï¿½tres avec un w correspondent aux paramï¿½tres de l'aile
+parameters.clmax        = 1.1 ;                             %                clmax 2D 
+%Les paramètres avec un w correspondent aux paramètres de l'aile
 parameters.bw           = 2 ;                               % [m]            envergure
-parameters.Sw           = 0.2 ;                             % [m^2]          surface ï¿½ l'air
-parameters.alphaw       = 2 ;                               % [degrï¿½]        angle d'incidence
-parameters.tcw          = 0.087 ;                           %                ï¿½paisseur max    
-parameters.xcw          = 0.302 ;                           %                position de l'ï¿½paisseur max   
-parameters.Dw           = 5 ;                               % [degrï¿½]        angle de dihï¿½dre
-parameters.Q            = 1 ;                               %                coefficient d'interfï¿½rence
-%Les paramï¿½tres avec un vt correspondent aux paramï¿½tres de l'empennage
+parameters.Sw           = 0.2 ;                             % [m^2]          surface à l'air
+parameters.alphaw       = 2 ;                               % [degré]        angle d'incidence
+parameters.tcw          = 0.087 ;                           %                épaisseur max    
+parameters.xcw          = 0.302 ;                           %                position de l'épaisseur max   
+parameters.Dw           = 5 ;                               % [degré]        angle de dihèdre
+parameters.Q            = 1 ;                               %                coefficient d'interférence
+%Les paramètres avec un vt correspondent aux paramètres de l'empennage
 %horizontal
 parameters.Cvt          = 0.02; %ou 0.04                    %                coefficient d'empennage vertical
 parameters.Cht          = 0.5 ;                             %                coefficient d'empennage horizontal
-parameters.tcht         = 0.15 ;                            %                ï¿½paisseur max    
-parameters.xcht         = 0.30 ;                            %                position de l'ï¿½paisseur max
+parameters.tcht         = 0.15 ;                            %                épaisseur max    
+parameters.xcht         = 0.30 ;                            %                position de l'épaisseur max
 parameters.lht          = 1 ;                               % [m]            distance aile/empennage
-parameters.Dht          = 0 ;                               % [degrï¿½]        angle de dihï¿½dre
-%Les paramï¿½tres avec un vt correspondent aux paramï¿½tres de l'empennage
+parameters.Dht          = 0 ;                               % [degré]        angle de dihèdre
+%Les paramètres avec un vt correspondent aux paramètres de l'empennage
 %verticale
 parameters.lvt          = 1 ;                               % [m]            distance aile/empennage
-parameters.tcvt         = 0.15 ;                            %                ï¿½paisseur max    
-parameters.xcvt         = 0.30 ;                            %                position de l'ï¿½paisseur max
-parameters.Dvt          = 0 ;                               % [degrï¿½]        angle de dihï¿½dre
-%Les paramï¿½tres avec un f correspondent aux paramï¿½tres du fuselage 
+parameters.tcvt         = 0.15 ;                            %                épaisseur max    
+parameters.xcvt         = 0.30 ;                            %                position de l'épaisseur max
+parameters.Dvt          = 0 ;                               % [degré]        angle de dihèdre
+%Les paramètres avec un f correspondent aux paramètres du fuselage 
 %parameters.lf           = ;                                 % [m]            longueur du fuselage
-%parameters.df           = ;                                 % [m]            diamï¿½tre du fuselage
-%parameters.Sf           = ;                                 % [m^2]          surface ï¿½ l'air fuselage        
+%parameters.df           = ;                                 % [m]            diamètre du fuselage
+%parameters.Sf           = ;                                 % [m^2]          surface à l'air fuselage        
 save('param.mat','parameters')
-

8d39be1ac9f3222da63b09812b1bcf81b0b60a8f
Audrey3131 <61624668+Audrey3131@users.noreply.github.com>
2020-03-26 16:52:51 -0400

    Update main.m

--- src/main.m
+++ src/main.m
@@ -1,4 +1,14 @@
 clc
 clear 
+clear qll
+
+load param.mat
+
+[E] = Endurance();
+[X,x1,x2,x,v,t] = LFL();
+[Hn_fixed, Hn_free] = LongitudinalStaticStability();
+[CnB_directional, CnB_lateral] = DirectionalLateralStaticStability();
+[V0y, V0x, M0y, M0x] = moment() ;
+[Cd0_w,Cd0_HT, Cd0_VT, Cd0_F] = Parasitedrag();
 
 disp('try')

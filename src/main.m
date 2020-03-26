200031e3c7a0c59da3d7d7c7c688281c136f2a94
lmstrm <61624861+lmstrm@users.noreply.github.com>
2020-03-26 13:09:51 -0400

    Update main.m

--- src/main.m
+++ src/main.m
@@ -1,8 +1,11 @@
 clc
 clear 
-
+clear all
 load param.mat
 
-[E] = Endurance()
+[E] = Endurance();
+[X,x1,x2,x,v,t] = LFL();
+[Hn_fixed, Hn_free] = LongitudinalStaticStability();
+[CnB_directional, CnB_lateral] = DirectionalLateralStaticStability();
 
 disp('try')

01d37e758bcc834305e83a114147f43c9cadc79e
lmstrm <61624861+lmstrm@users.noreply.github.com>
2020-03-26 17:43:44 -0400

    Update main.m

--- src/main.m
+++ src/main.m
@@ -7,6 +7,6 @@ load param.mat
 [X,x1,x2,x,v,t] = LFL();
 [Hn_fixed, Hn_free] = LongitudinalStaticStability();
 [CnB_directional, CnB_lateral] = DirectionalLateralStaticStability();
-[V0y, V0x, M0y, M0x] = moment() ;
+[V0y, V0x, M0y, M0x] = moment();
 
 disp('try')

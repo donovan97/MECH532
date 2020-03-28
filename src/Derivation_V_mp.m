ff200087c75168d8e229a00e2ab2f9af0f0fc75d
lmstrm <61624861+lmstrm@users.noreply.github.com>
2020-03-28 12:19:29 -0400

    Update Derivation_V_mp.m

--- src/Derivation_V_mp.m
+++ src/Derivation_V_mp.m
@@ -1,25 +1,27 @@
-function [V_mp] = Derivation_V_mp()
+function [] = Derivation_V_mp()
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 load param.mat
-v=[sqrt(2*parameters.Wto*parameters.g/(parameters.rho*parameters.Sw*parameters.CLmax))];
-dv=0.01;  %
-[CD0,k] = Parasitedrag()
-cl=[parameters.CLmax];
-cd=[(CD0+k*parameters.CLmax*parameters.CLmax)];
-n=1000;
-cl32_cd=[cl(end)^(3/2)/cd(end)]
-for k=1:n
-    v=[v v(end)+dv];
-    CL=2*parameters.Wto*parameters.g/(parameters.rho*parameters.Sw*v(end)*v(end)) ;
-    CD=CD0+k*(CL^2);
-    cl=[cl CL];
-    cd=[cd CD];
-    cl32_cd=[cl32_cd cl(end)^(3/2)/cd(end)];
+V0=sqrt(2*parameters.Wto*parameters.g/(parameters.rho*parameters.Sw*parameters.CLmax));
+n=10;
+[Cd0,k] = Parasitedrag();
+CLMAX=parameters.CLmax
+cl=linspace(0,CLMAX,n);
+cd=linspace(0,CLMAX,n);
+
+
+
+for i=1:length(cl)
+    cl(i)
+    cd(i)=(Cd0+k*(cl(i)^2))
+    
+    %cl32_cd=[cl32_cd cl(end)^(3/2)/cd(end)];
 end
-plot(cl,cd)
-figure(3)
-plot(v,cd)
+plot(cd,cl)
+hold on
+% figure(3)
+% plot(v,cd)
+% hold on
 
 %plot(v,cl32_cd)
   

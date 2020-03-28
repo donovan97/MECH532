43571174c1141d2a7a1185e6aca0bd922558dade
lmstrm <61624861+lmstrm@users.noreply.github.com>
2020-03-28 13:29:15 -0400

    Update Derivation_CL_mp.m

--- src/Derivation_CL_mp.m
+++ src/Derivation_CL_mp.m
@@ -4,8 +4,6 @@ function [CLmd] = Derivation_CL_mp()
 load param.mat
 V0=sqrt(2*parameters.Wto*parameters.g/(parameters.rho*parameters.Sw*parameters.CLmax));
 n=1000;
-[Cd0,k] = Parasitedrag();
-Cd0=Cd0/6;
 CLMAX=parameters.CLmax
 cl=linspace(0,CLMAX,n);
 cd=linspace(0,CLMAX,n);
@@ -13,6 +11,7 @@ cl32_cd=linspace(0,CLMAX,n);
 
 
 for i=1:length(cl)
+    [Cd0,k] = Parasitedrag(cl(i));
     cl(i);
     cd(i)=(Cd0+k*(cl(i)^2));
     
@@ -23,7 +22,7 @@ hold on
 % figure(3)
 % plot(v,cd)
 % hold on
-
+figure(3)
 plot(cl,cl32_cd)
 [cl32_cd_max,index]=max(cl32_cd)
 Clmd=cl(index)

6e078ad1dbf2629327ccef8f3e5b601317116f6a
lmstrm <61624861+lmstrm@users.noreply.github.com>
2020-04-07 19:34:08 -0400

    Update Optimisation_Endurance.m

--- src/Optimisation_Endurance.m
+++ src/Optimisation_Endurance.m
@@ -1,20 +1,37 @@
-function [Endurance,c,b] = Optimisation_Endurance(n)
+n=6;
 
 
 Param_def
 
-ct=linspace(0.05,0.3,n);
+ct=linspace(0.05,1,n);
 b=linspace(1.5,2,n);
 E=zeros(n,n);
+
 for i=1:n
+    i
     for j=1:n
      parameters.Ct=ct(i);
-     parameters.b=b(j);
+     parameters.Co=ct(i);
+     parameters.bw=b(j);
+     parameters.Sw=parameters.Ct*parameters.bw;
      save('param.mat','parameters')
+     
     [CLmd,CDmd, Cd0, k] = Derivation_CL_mp(0);
-     E(i,j)= Endurance();
+    parameters.Cl=CLmd;
+    parameters.Cd=CDmd;
+    parameters.Cd0=Cd0;
+    parameters.k=k;
+    save('param.mat','parameters')
+    %Vst=sqrt(2*parameters.Wto/(parameters.rho*parameters.Sw))*(parameters.n/parameters.Cl)^(3/2)*parameters.Cd 
+    
+    
+    
+    E(i,j)=Endurance(0);
+    
+    
+    
+    
+   
     end
 end
 surf(ct,b,E);
-end
-

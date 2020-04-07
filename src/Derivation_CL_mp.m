2c23311c5029af20cb8d6fe9c0a59ed549b00182
lmstrm <61624861+lmstrm@users.noreply.github.com>
2020-04-07 19:34:30 -0400

    Update Derivation_CL_mp.m

--- src/Derivation_CL_mp.m
+++ src/Derivation_CL_mp.m
@@ -1,9 +1,9 @@
-function [CLmd,CDmd, Cd0, k] = Derivation_CL_mp()
+function [CLmd,CDmd, Cd0, k] = Derivation_CL_mp(disp)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 load param.mat
 %V0=sqrt(2*parameters.Wto*parameters.g/(parameters.rho*parameters.Sw*parameters.CLmax));
-n=100;
+n=50;
 CLMAX=parameters.CLmax;
 cl=linspace(0,CLMAX,n);
 cd=linspace(0,CLMAX,n);
@@ -16,18 +16,19 @@ for i=1:length(cl)
     cl32_cd(i)=cl(i)^(3/2)/cd(i);
 end
 
-
-figure(1)
-plot(cd,cl)
-title("Cl over Cd")
-xlabel('Cd')
-ylabel('Cl')
-hold on
-figure(2)
-plot(cl,cl32_cd)
-title("Minimum power conditions")
-xlabel('Cl')
-ylabel('Cl^{3/2}/Cd')
+if disp==1
+    figure(1)
+    plot(cd,cl)
+    title("Cl over Cd")
+    xlabel('Cd')
+    ylabel('Cl')
+    hold on
+    figure(2)
+    plot(cl,cl32_cd)
+    title("Minimum power conditions")
+    xlabel('Cl')
+    ylabel('Cl^{3/2}/Cd')
+end
 [cl32_cd_max,index]=max(cl32_cd);
 CLmd=cl(index);
 CDmd=cd(index);

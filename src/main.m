9b045879d69e2fcf3bd4728b0bfd9220243e4551
lmstrm <61624861+lmstrm@users.noreply.github.com>
2020-04-07 17:59:10 -0400

    Update main.m

--- src/main.m
+++ src/main.m
@@ -7,8 +7,8 @@ Param_def
 [CLmd,CDmd, Cd0, k] = Derivation_CL_mp();
 [E] = Endurance();
 [X,x1,x2,x,v,t] = LFL();
-[Hn_fixed, Hn_free] = LongitudinalStaticStability();
-[CnB_directional, CnB_lateral] = DirectionalLateralStaticStability();
+[Hn_fixed, Hn_free] = LongitudinalStaticStability(1);
+[CnB_directional, CnB_lateral] = DirectionalLateralStaticStability(1);
 [V0y, V0x, M0y, M0x, margin_tensile, margin_compressive] = moment();
 
 load param.mat

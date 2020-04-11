a9a30dae24448c9d63621cf58f556dbd55b7315d
lmstrm <61624861+lmstrm@users.noreply.github.com>
2020-04-10 21:58:52 -0400

    Update main.m

--- src/main.m
+++ src/main.m
@@ -1,7 +1,7 @@
 clc
 clear all
 close all
-
+global parameters
 Param_def
 
 [CLmd,CDmd, Cd0, k] = Derivation_CL_mp(1);
@@ -9,6 +9,6 @@ Param_def
 [X] = LFL(1);
 [Hn_fixed, Hn_free] = LongitudinalStaticStability(1);
 [CnB_directional, CnB_lateral] = DirectionalLateralStaticStability(1);
-[V0y, V0x, M0y, M0x, margin_tensile, margin_compressive] = moment();
+[V0y, V0x, M0y, M0x, margin_tensile, margin_compressive,structure_test] = moment(1);
 
-load param.mat
+

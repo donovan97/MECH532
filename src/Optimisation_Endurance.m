7f782cc4dbec0535a1df257d710a5f7051d856f1
lmstrm <61624861+lmstrm@users.noreply.github.com>
2020-04-23 14:48:53 -0400

    Update Optimisation_Endurance.m

--- src/Optimisation_Endurance.m
+++ src/Optimisation_Endurance.m
@@ -2,7 +2,7 @@ close all
 clear all
 global parameters
 Param_def;
-n=100;
+n=20;
 ct_min=0.05;
 ct_max=0.3;
 b_min=1;
@@ -43,12 +43,10 @@ for i=1:n
 
     for j=1:n
         parameters.Ct=ct(i,j);
-        parameters.Ct;
         parameters.Co=ct(i,j);                
         parameters.bw=b(i,j);
-        parameters.bw;
         parameters.Sw=parameters.Ct*parameters.bw;
-        parameters.L= 0.5*parameters.bw;  
+        parameters.L            = 0.5*parameters.bw;  
         parameters.lambda  		= parameters.Ct/parameters.Co;
         parameters.Cvt          = (parameters.Svt*parameters.lt)/(parameters.bw*parameters.Sw) ;
         parameters.Cht          = (parameters.Sht*parameters.lt)/(parameters.c*parameters.Sw)   ;
@@ -56,9 +54,10 @@ for i=1:n
         parameters.t       		= 0.12*parameters.Co;
         parameters.h       		= 0.02*parameters.Co; 
        % test=test+1;
-
-        
-       % test1=test
+        parameters.Wws     		= 30*parameters.bw*pi*parameters.t*parameters.Co/4 ;  					    % [kg]           Weight of wing structure
+        parameters.Wfus    		= 0.19+1.5; 						% [kg]           Weight of the fuselage
+        parameters.Wto     		= parameters.Wws +parameters.Wfus;  % [kg]           Total aircraft weight
+        parameters.Wto 
 
         [CLmp,CDmp, Cd0, k] = Derivation_CL_mp(0);
         parameters.Cl=CLmp;
@@ -67,6 +66,9 @@ for i=1:n
         
         parameters.Cd0=Cd0;
         parameters.k=k;
+        V=sqrt(2*parameters.Wto*parameters.g/(parameters.rho*parameters.Sw*parameters.Cl));
+        parameters.drag = 0.5*parameters.rho*parameters.Sw*V*V*parameters.Cd;
+
        % test=test+1;
        % test2=test
         %Vst=sqrt(2*Wto/(rho*Sw))*(n/Cl)^(3/2)*Cd 
@@ -95,14 +97,14 @@ for i=1:n
             iter2=iter2+1;
         end
         
-        if Hn_fixed <= 0 | Hn_free <= 0
+        if Hn_fixed <= 0 || Hn_free <= 0
             ct_failure(3,iter3)=ct(i,j);
             b_failure(3,iter3)=b(i,j);
             E_failure(3,iter3)=Endu;
             iter3=iter3+1;
         end
             
-        if CnB_directional <= 0 | CnB_lateral >= 0
+        if CnB_directional <= 0 || CnB_lateral >= 0
             ct_failure(4,iter4)=ct(i,j);
             b_failure(4,iter4)=b(i,j);
             E_failure(4,iter4)=Endu;
